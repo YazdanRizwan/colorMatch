6dbbcad1ee10451a22bf6f370bb97d2c22fa04b1
YAZDAN <19btcs008hy@manuu.edu.in>
2023-03-28 15:02:33 +0530

    New Project

---
+++ script.js
@@ -0,0 +1,120 @@
+
+var errors = 0;
+var cardList = [
+    "Red",
+    "yellow",
+    "green",
+    "purple",
+    "orange",
+    "blue",
+    "gray",
+    "brown"
+]
+
+var count = 0;
+var cardSet;
+var board = [];
+var rows = 4;
+var columns = 4;
+var card1Selected;
+const myButton = document.getElementById("btn1");
+var card2Selected;
+window.onload = function () {
+    shuffleCards();
+    startGame();
+}
+
+let time = prompt("Please enter the amount of time for which you want to show color in the starting in seconds!");
+
+function shuffleCards() {
+    cardSet = cardList.concat(cardList); //two of each card
+    console.log(cardSet);
+    //shuffle
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); //get random index
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
+}
+
+function startGame() {
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let cardImg = cardSet.pop();
+            row.push(cardImg);
+            let card = document.createElement("img");
+            card.id = r.toString() + "-" + c.toString();
+            card.src = cardImg + ".jpg";
+            card.classList.add("card");
+            card.addEventListener("click", selectCard);
+            document.getElementById("board").append(card);
+
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+    setTimeout(hideCards, time*1000);
+}
+
+function hideCards() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let card = document.getElementById(r.toString() + "-" + c.toString());
+            card.src = "back.jpg";
+        }
+    }
+}
+
+function selectCard() {
+
+    if (this.src.includes("back")) {
+        if (!card1Selected) {
+            card1Selected = this;
+
+            let coords = card1Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card1Selected.src = board[r][c] + ".jpg";
+        }
+        else if (!card2Selected && this != card1Selected) {
+            card2Selected = this;
+
+            let coords = card2Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card2Selected.src = board[r][c] + ".jpg";
+            setTimeout(update, 1000);
+        }
+    }
+
+}
+
+function update() {
+    if (card1Selected.src != card2Selected.src) {
+        card1Selected.src = "back.jpg";
+        card2Selected.src = "back.jpg";
+        errors += 1;
+        document.getElementById("errors").innerText = errors;
+    }
+    else {
+        count += 1;
+        console.log(count);
+        if (count === 8) {
+            myButton.style.display = "block";
+            myButton.style.position = "absolute";
+            myButton.style.left = "50%";
+            myButton.style.transform = "translateX(-50%)";
+        }
+    }
+
+    card1Selected = null;
+    card2Selected = null;
+}
+
